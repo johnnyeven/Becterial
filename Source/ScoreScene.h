76b02807e2f748bac3de44d7e5b101b3e19653ef
李翌文 <johnnyeven@gmail.com>
2014-06-30 21:03:47 +0800

    老规则的备份1

---
+++ Source/ScoreScene.h
@@ -0,0 +1,13 @@
+//
+//  ScoreScene.h
+//  bacterial
+//
+//  Created by 李翌文 on 14-6-30.
+//  Copyright (c) 2014年 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface ScoreScene : CCNode
+
+@end
---
+++ Source/ScoreScene.m
@@ -0,0 +1,18 @@
+//
+//  ScoreScene.m
+//  bacterial
+//
+//  Created by 李翌文 on 14-6-30.
+//  Copyright (c) 2014年 Apportable. All rights reserved.
+//
+
+#import "ScoreScene.h"
+
+@implementation ScoreScene
+
+-(void)share
+{
+    
+}
+
+@end

45d4664ec7e2a7f1d3138a6a7212f24dcc2e4fd6
johnnyeven_work <johnnyeven@gmail.com>
2014-07-03 17:26:52 +0800

    1

---
+++ Source/AWScreenshot.h
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface AWScreenshot : NSObject
+{
+}
+
++ (CGImageRef) takeAsCGImage;
+
+#ifdef __IPHONE_OS_VERSION_MAX_ALLOWED
++ (UIImage*) takeAsImage;
+#else
++ (CGImageRef) takeAsImage;
+#endif
+
++ (CCTexture2D*) takeAsTexture;
+
+@end
